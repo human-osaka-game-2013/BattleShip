079e5d94cdeb66f99974a5cd7176b9e5e629b4e6
tomoaki-kameda <tomoaki-kameda@c0c35299-66f9-4b7c-b7c0-c64df79a37b7>
2015-08-24 07:53:05 +0000

    盤面表示のオブジェクトクラス、 BoardOfFrameのヘッダーをインクルード忘れ。

---
+++ BattleShip/BattleShip/scr/BoardOfFrame.h
@@ -0,0 +1,53 @@
+
+
+#ifndef _BOARDOFFRAME_H_
+#define _BOARDOFFRAME_H_
+
+#include "gameObject.h"
+/**
+*@brief	盤面のフレームオブジェクトクラス（描画用）
+*@details	ステージのUI要素などを別のクラスとして持たせる。@n
+			ステージやプレイヤー情報は必要に応じてメンバからもらう。
+*/
+class BoardOfFrame : public CGameObject
+{
+private:
+	float fWidth ,fHeight;
+
+public:
+	/**
+	*@brief	初期化
+	*/
+	void Init( float _fx, float _fy, float _fWidth, float _fHeight )
+	{
+		SetPosition(_fx, _fy, 0.5f);
+		fHeight = _fHeight;
+		fWidth = _fWidth;
+	}
+	void Control(){};
+	void Draw(){};
+	void Free(){};
+	float GetWidth(){ return fWidth; }
+	float GetHeight(){ return fHeight; }
+
+	/**
+	*@brief	当たり判定処理
+	*@details	引数内のxy座標が、自身のオブジェクト内に入っているかを判定
+	*@param[in]	_x	x座標
+	*@param[in]	_y	y座標
+	*/
+	bool HitBlockCheck( float _x, float _y )
+	{
+		float tempX = GetPositionX(), tempY = GetPositionY();
+		if( tempX < _x && tempX + fWidth > _x )
+		{
+			if(	tempY < _y && tempY + fHeight > _y )
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+#endif
